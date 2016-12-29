cfd9d6a10b26ca0107cf5e9ddb71d3f192eff29c
Tung Pham Thanh <Tung Pham Thanh>
2016-12-29 23:29:29 +0700

    update to ES6

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-var path = require('path');
+'use strict';
+const path = require('path');
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Types.ObjectId;
 
-var ERR = {
-	API_WEAK: 'APIKEY INVALID',
-	AUTH_ERROR: 'AUTHENTICATION MISSING'
+const ERR = {
+	ELEVEL: 'APIKEY INVALID',
+	EAUTH: 'AUTHENTICATION MISSING'
 };
 
-var HELP_OBJ = {
+const HELP_OBJ = {
 	"body": {
 		"id": "_id",
 		"q": "query",
@@ -36,17 +39,16 @@ var HELP_OBJ = {
 	}
 };
 
-var _reviverISODate = function (key, value) { // new Date().toISOString() = "2015-12-16T09:17:06.307Z"
-    var a;
-    if (typeof value === 'string') {
-        a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
-        if (a) {
-            return new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4], +a[5], +a[6]));
-        }
-    }
-    return value;
-}
-var _parseJSON = function (obj, reviver) {
+const _reviverISODate = (key, value) => { // new Date().toISOString() = "2015-12-16T09:17:06.307Z"
+	if (typeof value != 'string') return value;
+
+	let a = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*)?)Z$/.exec(value);
+	if (!a) return value;
+
+	return new Date(Date.UTC(+a[1], +a[2] - 1, +a[3], +a[4], +a[5], +a[6]));
+};
+
+const _parseJSON = (obj, reviver) => {
 	try {
 		if (!obj) return null;
 
@@ -59,23 +61,24 @@ var _parseJSON = function (obj, reviver) {
 				return null;
 		}
 	} catch (ex) {
-		console.log(ex + '\nObject is not valid JSON string:', obj);
+		console.log(`${ex}\nObject is not valid JSON string:`, obj);
 		return null;
 	}
-}
-var _parseDeepPop = function (obj) {
+};
 
-	var resPath = [];
-	var resOption = null;
+const _parseDeepPop = obj => {
 
-	var parseSingleObj = function (dp) {
+	let resPath = [];
+	let resOption = null;
+
+	const parseSingleObj = dp => {
 		if (!dp.path) return;
 
-		var path = dp.path; delete dp.path;
+		const path = dp.path; delete dp.path;
 
 		resPath.push(path);
 		resOption.populate[path] = dp;
-	}
+	};
 
 	switch (typeof obj) {
 		case 'string':
@@ -91,7 +94,7 @@ var _parseDeepPop = function (obj) {
 					resPath = obj;
 					resOption = null;
 				} else {
-					for (var i = 0; i < obj.length; i++) {
+					for (let i = 0; i < obj.length; i++) {
 						parseSingleObj(obj[i])
 					};
 				}
@@ -108,230 +111,221 @@ var _parseDeepPop = function (obj) {
 		path: resPath,
 		option: resOption
 	};
-}
+};
 
-var _A = {};
+const _A = {
+	ROUTE: '/acrud/:model/:action/:ressoon?',
+	OPTIONS: {},
+	CMD: {},
+	ACL: {},
+};
 
-_A.O = {};
-_A.CMD = {};
-
-_A.init = function(option, actionFunctions) {
+_A.init = (option, cmds, acl) => {
 	option = option || {};
+	_A.O = _A.OPTIONS;
 
-	_A.O.route = option.route || '/acrud/:model/:action/:ressoon?';
 	_A.O.keystone = option.keystone || null;
+
 	_A.O.schemaFolder = option.schemaFolder || './schemas';
 	_A.O.schemaHash = option.schemaHash || {};
-	console.log('_A.O', JSON.stringify(_A.O));
+	// console.log('_A.O', JSON.stringify(_A.O));
 
-	_A.O.mongoose = option.mongoose || require('mongoose');
+	_A.O.mongoose = option.mongoose || mongoose;
 	_A.O.deepPopulate = require('mongoose-deep-populate')(_A.O.mongoose);
-	_A.O.ObjectId = _A.O.mongoose.Types.ObjectId;
 
 	_A.OPTIONS = _A.O;
-	_A.CMD = actionFunctions;
+	_A.CMD = cmds || {};
+	_A.ACL = acl || {};
 }
 
-_A.controller = function(req, res) {
+_A.controller = (req, res) => {
+	const reqID = new Date().getTime();
 
-	try {
-		// var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-		//**/console.log('ip', ip);
+	const action = req.params.action;
+	const model = req.params.model;
+	const resSoon = req.params.ressoon == 'true'  || req.params.ressoon == '1';
 
-		var reqID = new Date().getTime();
+	const body = req.body;
 
-		var action = req.params.action;
-		var model = req.params.model;
-		var resSoon = req.params.ressoon == 'true'  || req.params.ressoon == 1;
+	const AUTH_KEY = req.headers.authorization;
+	const LEVEL_KEY = Number( _A.ACL[AUTH_KEY] ? (_A.ACL[AUTH_KEY][model] || 0) : (process.env[AUTH_KEY] || 0) );
 
-		var body = req.body;
-		var sentAPPNAME = req.headers.APPNAME || req.headers.appname;
-		var sentAPIKEY = req.headers.authorization;
-		// sentAPIKEY = '_' + sentAPIKEY + '_'
-		var lvAPIKEY = ~~process.env[sentAPIKEY] || 0;
+	// console.log('AUTH_KEY, LEVEL_KEY', AUTH_KEY, LEVEL_KEY);
+	// console.log('req.params', JSON.stringify(req.params));
+	// console.log('req.body', JSON.stringify(req.body));
 
-		// console.log('sentAPIKEY, lvAPIKEY', sentAPIKEY, lvAPIKEY);
+	const fnResult = (err, result, result2) => {
+		if (err) console.log(`RESULT_ERR=${err}`);
 
-		//**/console.log('body', body);
-		//**/console.log('root', root);
+		if (!res.headersSent)
+			res.json({err, result, result2});
+	};
 
-		var id = body.id; //_id
-		var q = body.q; //query
-		var s = body.s; //sort
-		var sk = body.sk; //skip
-		var u = body.u; //update
-		var o = body.o; //option
-		var l = body.l; //limit
-		var f = body.f; //field
-		var p = body.p; //populate
-		var dp = body.dp; //deep populate
+	const fnVoid = (err, result, result2) => {
+		if (err) console.log(`VOID_ERR=${err} ${result} ${result2}`);
+	};
 
-		var KSList = _A.O.keystone ? _A.O.keystone.list(model) : null;
-		var Schema = _A.O.keystone ? KSList.schema : 
-						(_A.O.schemaHash[model] || require(path.join(_A.O.schemaFolder, model)));
+	if (!process.env[AUTH_KEY] || !LEVEL_KEY) {
+		console.log(ERR.EAUTH, LEVEL_KEY, JSON.stringify(body));
+		return fnResult(ERR.EAUTH);
+	}
 
-		if (dp) {
-			Schema.plugin(_A.O.deepPopulate);
-			dp = _parseDeepPop(dp);
-			console.log('dp=', JSON.stringify(dp));
+	const id = body.id; //_id
+	const q = body.q; //query
+	const s = body.s; //sort
+	const sk = body.sk; //skip
+	const u = body.u; //update
+	const o = body.o; //option
+	const l = body.l; //limit
+	const f = body.f; //field
+	const p = body.p; //populate
+	const dp = body.dp ? _parseDeepPop(body.dp) : null; //deep populate
+
+	let Model = null, Schema = null;
+
+	switch (true) {
+		case (!!_A.O.keystone): {
+			const KSList = _A.O.keystone.list(model);
+			Schema = KSList.schema;
+			if (dp) Schema.plugin(_A.O.deepPopulate);
+			Model = KSList.model;
+		} break;
+		default:{
+			Schema = (_A.O.schemaHash[model] || require(path.join(_A.O.schemaFolder, model)));
+			if (dp) Schema.plugin(_A.O.deepPopulate);
+			Model = _A.O.mongoose.model(model, Schema);
 		}
-		var Model = _A.O.keystone ? KSList.model : _A.O.mongoose.model(model, Schema);
+	}
 
-		var key = [model,action,q,s,sk,u,o,l,f,p].join('_');
+	const actionLow = action.toLowerCase();
+	switch (actionLow) {
+		case 'help':
+			return fnResult(null, HELP_OBJ);
+			break;
+		case 'save':
+			if (!(LEVEL_KEY&8)) return res.json({err: ERR.ELEVEL});
 
-		var fnResult = function (err, result, result2) {
-			if (err) console.log('MONGO_ERR=' + err);
+			const obj = body.toObject ? body.toObject() : body;
+			if (obj._id) {
+				const mrId = obj._id;
+				delete obj._id;delete obj.__v;
+				Model.findOneAndUpdate({_id: mrId}, obj, {new: true, upsert: true}, (err, doc) => resSoon
+					? fnVoid(err, doc, !err && doc ? 1 : 0)
+					: fnResult(err, doc, !err && doc ? 1 : 0));
 
-			if (!res.headersSent)
-				res.json({err: err, result: result, result2: result2});
-		}
+			} else {
+				var doc = new Model(obj);
+				doc.save(resSoon ? fnVoid : fnResult);
+			}
+			/*RES_SOON*/if (resSoon) return fnResult(null, doc, 1);
+			break;
+		case 'remove':
+			if (!(LEVEL_KEY&1)) return res.json({err: ERR.ELEVEL});
 
-		var fnVoid = function (err, result, result2) {
-			if (err) _utils.log('VOID_MONGO_ERR=' + err);
-			console.log('result='+reqID, JSON.stringify([result, result2]));
-		}
+			Model.remove(q, resSoon ? fnVoid : fnResult);
+			/*RES_SOON*/if (resSoon) return fnResult(null, 1);
+			break;
+		case 'find':
+			if (!(LEVEL_KEY&4)) return res.json({err: ERR.ELEVEL});
+			var query = Model.find(q);
+			if (sk) query.skip(sk);
+			if (l) query.limit(l);
+			if (f) query.select(f);
+			if (p) query.populate(p);
+			if (dp) query.deepPopulate(dp.path, dp.option);
+			if (s) query.sort(s);
+			query.exec(fnResult);
+			break;
+		case 'findid':
+			if (!(LEVEL_KEY&4)) return res.json({err: ERR.ELEVEL});
 
-		if (!process.env[sentAPIKEY] || !lvAPIKEY) {
-			console.log(ERR.AUTH_ERROR, sentAPPNAME, lvAPIKEY, JSON.stringify(body));
-			return fnResult(ERR.AUTH_ERROR, null, null);
-		}
+			if (!id) return res.json({err: `ID ${id} invalid`});
 
-		// /**/console.log('model='+model,'action='+action,'body='+reqID, JSON.stringify(body));
-		var actionLow = action.toLowerCase();
-		switch (actionLow) {
-			case 'help':
-				return fnResult(null, HELP_OBJ);
-				break;
-			case 'save':
-				if (!(lvAPIKEY&8)) return res.json({err: ERR.API_WEAK});
+			var query = Model.findOne({'_id': new ObjectId(id)});
 
-				var obj = body.toObject ? body.toObject() : body;
-				if (obj._id) {
-					var mrId = obj._id;
-					delete obj._id;delete obj.__v;
-					Model.findOneAndUpdate({_id: mrId}, obj, {new: true, upsert: true}, function (err, doc) {
-						return resSoon
-							? fnVoid(err, doc, !err && doc ? 1 : 0)
-							: fnResult(err, doc, !err && doc ? 1 : 0);
-					});
+			if (f) query.select(f);
+			if (p) query.populate(p);
+			if (dp) query.deepPopulate(dp.path, dp.option);
 
-				} else {
-					var doc = new Model(obj);
-					doc.save(resSoon ? fnVoid : fnResult);
-				}
-				/*RES_SOON*/if (resSoon) return fnResult(null, doc, 1);
-				break;
-			case 'remove':
-				if (!(lvAPIKEY&1)) return res.json({err: ERR.API_WEAK});
+			query.exec(fnResult);
+			break;
+		case 'findone':
+			if (!(LEVEL_KEY&4)) return res.json({err: ERR.ELEVEL});
 
-				Model.remove(q, resSoon ? fnVoid : fnResult);
-				/*RES_SOON*/if (resSoon) return fnResult(null, 1);
-				break;
-			case 'find':
-				if (!(lvAPIKEY&4)) return res.json({err: ERR.API_WEAK});
-				var query = Model.find(q);
-				if (sk) query.skip(sk);
-				if (l) query.limit(l);
-				if (f) query.select(f);
-				if (p) query.populate(p);
-				if (dp) query.deepPopulate(dp.path, dp.option);
-				if (s) query.sort(s);
-				query.exec(fnResult);
-				break;
-			case 'findid':
-				if (!(lvAPIKEY&4)) return res.json({err: ERR.API_WEAK});
+			var query = Model.findOne(q);
 
-				if (!id) return res.json({err: 'ID ' + id + ' invalid'});
+			if (s) query.sort(s);
+			if (sk) query.skip(sk);
+			if (l) query.limit(l);
+			if (f) query.select(f);
+			if (p) query.populate(p);
+			if (dp) query.deepPopulate(dp.path, dp.option);
 
-				var query = Model.findOne({'_id': new _A.O.ObjectId(id)});
+			query.exec(fnResult);
+			break;
+		case 'findoneandupdate':
+			if (!(LEVEL_KEY&2)) return res.json({err: ERR.ELEVEL});
 
-				if (f) query.select(f);
-				if (p) query.populate(p);
-				if (dp) query.deepPopulate(dp.path, dp.option);
+			if (!u) return res.json({err: `Update ${u} invalid`});
 
-				query.exec(fnResult);
-				break;
-			case 'findone':
-				if (!(lvAPIKEY&4)) return res.json({err: ERR.API_WEAK});
+			const opt = o ? o : {'new': true};
+			opt.new = true;
 
-				var query = Model.findOne(q);
+			Model.findOneAndUpdate(q, u, opt, fnResult);
+			break;
+		case 'count':
+			if (!(LEVEL_KEY&4)) return res.json({err: ERR.ELEVEL});
 
-				if (s) query.sort(s);
-				if (sk) query.skip(sk);
-				if (l) query.limit(l);
-				if (f) query.select(f);
-				if (p) query.populate(p);
-				if (dp) query.deepPopulate(dp.path, dp.option);
+			var query = Model.count(q);
 
-				query.exec(fnResult);
-				break;
-			case 'findoneandupdate':
-				if (!(lvAPIKEY&2)) return res.json({err: ERR.API_WEAK});
+			if (s) query.sort(s);
+			if (sk) query.skip(sk);
+			if (l) query.limit(l);
+			if (f) query.select(f);
 
-				if (!u) return res.json({err: 'Update ' + u + ' invalid'});
+			query.exec(fnResult);
+			break;
+		case 'update':
+			if (!(LEVEL_KEY&2)) return res.json({err: ERR.ELEVEL});
 
-				var opt = o ? o : {'new': true};
-				opt.new = true;
+			if (!u) return res.json({err: `Update ${u} invalid`});
+			Model.update(q, u, o ? o : null, resSoon ? fnVoid : fnResult);
+			/*RES_SOON*/if (resSoon) return fnResult(null, 1);
+			break;
+		case 'updateid':
+			if (!(LEVEL_KEY&2)) return res.json({err: ERR.ELEVEL});
 
-				Model.findOneAndUpdate(q, u, opt, fnResult);
-				break;
-			case 'count':
-				if (!(lvAPIKEY&4)) return res.json({err: ERR.API_WEAK});
+			if (!id) return res.json({err: `ID ${id} invalid`});
+			if (!u) return res.json({err: `Update ${u} invalid`});
 
-				var query = Model.count(q);
+			Model.findOneAndUpdate({'_id': new ObjectId(id)}, u, o ? o : null, fnResult);
+			break;
+		case 'aggregate':
+			if (!(LEVEL_KEY&16)) return res.json({err: ERR.ELEVEL});
 
-				if (s) query.sort(s);
-				if (sk) query.skip(sk);
-				if (l) query.limit(l);
-				if (f) query.select(f);
+			let aggr = body;
+			if (typeof body == 'string') {
+				aggr = _parseJSON(body, _reviverISODate);
+			} else if (typeof body == 'object') {
+				aggr = _parseJSON(JSON.stringify(body), _reviverISODate);
+			}
 
-				query.exec(fnResult);
-				break;
-			case 'update':
-				if (!(lvAPIKEY&2)) return res.json({err: ERR.API_WEAK});
+			// console.log('### aggr=', JSON.stringify(aggr));
+			// console.log('### match=', typeof aggr[0]['$match']['createdAt']['$gt']);
 
-				if (!u) return res.json({err: 'Update ' + u + ' invalid'});
-				Model.update(q, u, o ? o : null, resSoon ? fnVoid : fnResult);
-				/*RES_SOON*/if (resSoon) return fnResult(null, 1);
-				break;
-			case 'updateid':
-				if (!(lvAPIKEY&2)) return res.json({err: ERR.API_WEAK});
+			var query = Model.aggregate(aggr);
+			query.exec(fnResult);
+			break;
+		default:
+			if (_A.CMD && _A.CMD[actionLow]) {
+				return _A.CMD[actionLow](req, res, {
+					model: Model, 
+					authKey: AUTH_KEY,
+					levelKey: LEVEL_KEY
+				});
+			}
 
-				if (!id) return res.json({err: 'ID ' + id + ' invalid'});
-				if (!u) return res.json({err: 'Update ' + u + ' invalid'});
-
-				Model.findOneAndUpdate({'_id': new _A.O.ObjectId(id)}, u, o ? o : null, fnResult);
-				break;
-			case 'aggregate':
-				if (!(lvAPIKEY&4)) return res.json({err: ERR.API_WEAK});
-
-				var aggr = body;
-				if (typeof body == 'string') {
-					aggr = _parseJSON(body, _reviverISODate);
-				} else if (typeof body == 'object') {
-					aggr = _parseJSON(JSON.stringify(body), _reviverISODate);
-				}
-
-				// _utils.log('### aggr=', _utils.strJSON(aggr));
-				// _utils.log('### match=', typeof aggr[0]['$match']['createdAt']['$gt']);
-
-				var query = Model.aggregate(aggr);
-				query.exec(fnResult);
-				break;
-			default:
-				if (_A.CMD && _A.CMD[actionLow]) {
-					return _A.CMD[actionLow](req, res, {
-						model: Model, 
-						authKey: sentAPIKEY,
-						levelKey: lvAPIKEY
-					});
-				}
-
-				return res.json({err: 'Action ' + action + ' not found'});
-		}
-	} catch (ex) {
-		console.log(ex);
-		return res.json({err: ex.toString() + ex.stack});
+			return res.json({err: `Action ${action} not found`});
 	}
 };
 
--- test.js
+++ test.js
@@ -1,28 +1,27 @@
+'use strict';
 require('dotenv').load();
-var path = require('path');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://testuser:passtestacrud@candidate.36.mongolayer.com:10708/jmlog', function (err) {
+
+const path = require('path');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://testuser:passtestacrud@candidate.36.mongolayer.com:10708/jmlog', err => {
 	console.log('mongoose.connect', err);
 });
 
-var express = require('express');
+const express = require('express');
+const app = express();
 
-/*ACRUD*/
-var acrud = require('./index.js');
-acrud.init({
-	mongoose: mongoose,
-	schemaFolder: path.join(__dirname, 'schemas')
-});
-
-var app = express();
-
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 /*ACRUD*/
-app.post(acrud.OPTIONS.route, acrud.controller);
+const acrud = require('./index.js');
+acrud.init({
+	mongoose: mongoose,
+	schemaFolder: path.join(__dirname, 'schemas')
+});
+app.post(acrud.ROUTE, acrud.controller);
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!')
-})
+app.listen(3000, () => {
+	console.log('Listening on port: 3000')
+});
